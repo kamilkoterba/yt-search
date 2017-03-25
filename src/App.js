6a1f23f9e979b34997ce96c14ee540430a7fc4c4
Kamil Koterba <kkoterba@egnyte.com>
2017-03-25 23:15:02 +0100

    Refactor App functional component to class conponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/SearchBar";
 
-const App = () => (
-    <div>
-        <SearchBar />
-    </div>
-);
+const API_KEY = "AIzaSyAUJJ2QvFRF4N-_EtIGAIy-0H4nT4UEFtI";
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+        YTSearch({ key: API_KEY, term: "volvo" }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 export default App;
