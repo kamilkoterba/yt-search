874b1abe563f2be3550b27ac105a72c9e6e7f34c
Kamil Koterba <kkoterba@egnyte.com>
2017-03-25 23:08:09 +0100

    Input controlled component, YT api call

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { term: "" };
+        this.state = { term: "Initial state" };
     }
 
     render() {
         return (
             <div>
-                <input onChange={ (event) => this.setState({ term: event.target.value }) } />
+                <input
+                    value={ this.state.term }
+                    onChange={ (event) => this.setState({ term: event.target.value }) }
+                />
                 Value of the input { this.state.term }
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from "youtube-api-search";
 import App from "./App";
+
+const API_KEY = "AIzaSyAUJJ2QvFRF4N-_EtIGAIy-0H4nT4UEFtI";
+YTSearch({key: API_KEY, term: "volvo"}, (response) => {
+    console.log(response);
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
