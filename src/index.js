ffdca1e59a437f9635961d65b39dd4042afc68f0
Kamil Koterba <kkoterba@egnyte.com>
2017-03-25 19:53:38 +0100

    First functional component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hi</div>, document.getElementById("root"));
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
