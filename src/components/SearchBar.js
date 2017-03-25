f474f51e087cc4f11a5ac6b8a89e1c0056d75b44
Kamil Koterba <kkoterba@egnyte.com>
2017-03-25 22:16:04 +0100

    SearchBar class component and handle user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = () => (
-    <input type="text" />
-);
+class SearchBar extends Component {
+    render() {
+        return <input onChange={ this.onInputChange } />
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 export default SearchBar;
